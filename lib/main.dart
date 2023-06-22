025240915e968d4f2ff8d40e458a2ee5e3036deb
Muhammad Zaki <muhammadzaki@MacBook-Pro.local>
2023-06-23 00:02:10 +0800

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes2/views/login_view.dart';
+import 'package:mynotes2/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -14,6 +14,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/':(context) =>  const LoginView(),
+        '/register/':(context) =>  const RegisterView(),
+      },
     ),
   );
 }
@@ -23,29 +27,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
-            if (user?.emailVerified ?? false) {
-              print('You are verified');
-            } else {
-              print('You need to verify your email first');
-            }
-              return Text('Done');
+            // final user = FirebaseAuth.instance.currentUser;
+            // print(user);
+            // if (user?.emailVerified ?? false) {
+            //   return const Text('Done');
+            // } else {
+            //   return const VerifyEmailView(); 
+            // }
+              return const LoginView();
             default:
-              return const Text('Loading');
+              return const CircularProgressIndicator();
           }
         },
-      ),
-    );
+      );
   }
 }
+
