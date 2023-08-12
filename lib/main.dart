cb04af74baf66737097f15285cfb6fc64089eb1e
Muhammad Zaki <muhammadzaki@MacBook-Pro.local>
2023-08-12 12:50:19 +0800

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes2/views/login_view.dart';
 import 'package:mynotes2/views/register_view.dart';
 import 'package:mynotes2/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 import 'firebase_options.dart';
 
@@ -21,6 +21,7 @@ void main() {
       routes: {
         '/login/':(context) =>  const LoginView(),
         '/register/':(context) =>  const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -58,24 +59,31 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(email: email, password: password);
-                print(userCredential);
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                );
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/notes/',
+                  (route) => false,
+                );
+                // print(userCredential);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  print('User not found');
+                  devtools.log('User not found');
                 } else if (e.code == 'wrong-password') {
-                  print('Wrong password');
+                  devtools.log('Wrong password');
                 }
               }
             },
             child: const Text('Login'),
           ),
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route) => false);
-            }, 
-            child: const Text('Not registered yet? Register here'))
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/register/', (route) => false);
+              },
+              child: const Text('Not registered yet? Register here'))
         ],
       ),
     );
